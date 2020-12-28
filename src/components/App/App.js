212e30616050eac5a63842dbd96ab0c1f46e521f
Dmitriy <dm.min.zp@gmail.com>
2020-12-28 23:54:31 +0200

    fix bugs

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Start from '../Start/StartScreen';
 import MainBoard from '../MainBoard/MainBoard'
 
 
 function App() {
+
+  const [flag, setFlag] = useState(false)
+
+  const handleClick = () => {
+    setFlag(true)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Start />
-        <MainBoard />
+        {flag ?
+          <MainBoard /> :
+          <div onClick={handleClick}>
+            <Start />
+          </div>
+        }
       </header>
     </div>
   );
--- src/components/MainBoard/MainBoard.js
+++ src/components/MainBoard/MainBoard.js
@@ -6,7 +6,8 @@ class MainBoard extends React.Component {
       super(props);
       this.state = {
          squares: Array(9).fill(null),
-         count: 0
+         count: 0,
+         showButton: false,
       }
       // вложенный массив с победными комбинациями
       this.winnerLine = [
@@ -17,14 +18,14 @@ class MainBoard extends React.Component {
          [1, 4, 7],
          [2, 5, 8],
          [0, 4, 8],
-         [2, 4, 6]
+         [2, 4, 6],
       ]
    }
 
-   default = () => {
+   reset = () => {
       this.setState({ squares: Array(9).fill(null) });
-      this.setState({ count: 0 })
-      document.querySelector('#GameOver').style.display = "none";
+      this.setState({ count: 0 });
+      this.setState({ showButton: false });
    }
 
    isWinner = () => {
@@ -36,7 +37,7 @@ class MainBoard extends React.Component {
          if (!this.state.squares.includes(null)) {
             alert(' Ходов больше нет - НИЧЬЯ :`( ');
             setTimeout(() => {
-               document.querySelector('#GameOver').style.display = "block";
+               this.setState({ showButton: true });
             }, 500);
             break;
          }
@@ -46,7 +47,7 @@ class MainBoard extends React.Component {
             && this.state.squares[line[2]] === winLine) {
             alert(winLine + ' Победил');
             setTimeout(() => {
-               document.querySelector('#GameOver').style.display = "block";
+               this.setState({ showButton: true });
             }, 1500);
             break;
          }
@@ -72,25 +73,34 @@ class MainBoard extends React.Component {
       this.isWinner();
    }
 
+   renderSquares = () => {
+      return this.state.squares.map((item, index) => {
+         return (
+            <div
+               className="game-grid"
+               onClick={this.clickHandler}
+               data={index}
+               key={index}
+            >
+               {this.state.squares[index]}
+            </div>
+         )
+      })
+   }
+
    render() {
       return (
          <div className="MainBoard">
             <div>
-               <div className="game-grid" onClick={this.clickHandler} data="0">{this.state.squares[0]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="1">{this.state.squares[1]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="2">{this.state.squares[2]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="3">{this.state.squares[3]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="4">{this.state.squares[4]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="5">{this.state.squares[5]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="6">{this.state.squares[6]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="7">{this.state.squares[7]}</div>
-               <div className="game-grid" onClick={this.clickHandler} data="8">{this.state.squares[8]}</div>
+               {this.renderSquares()}
             </div>
-            <div>
-               <button id="GameOver" onClick={this.default}>
-                  Eще?
+            {this.state.showButton && (
+               <div>
+                  <button id="GameOver" onClick={this.reset}>
+                     Eще?
                </button>
-            </div>
+               </div>
+            )}
          </div>
       )
    }
